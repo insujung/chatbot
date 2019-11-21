050b2f5395e034254ca89896b4e9c8ba33dd9204
insujeong <wjddlstn0614@naver.com>
2019-11-21 06:39:22 +0000

    first commit

--- index1.js
+++ index1.js
@@ -1,16 +1,18 @@
 const { RTMClient } = require('@slack/rtm-api');
-var token = 'xoxb-832442583072-834656041622-ZvRvVlecrCOLYEGCeWjvYnZc';
 
-var rtm = new RTMClient(token);
+const token = 'xoxb-832442583072-834656041622-ZvRvVlecrCOLYEGCeWjvYnZc';
+
+const rtm = new RTMClient(token);
 rtm.start();
 
-rtm.on('message', function (message){
-	var channel = message.channel;
-	var text = message.text;
-
-	if (text == '안녕') {
-		rtm.sendMessage('헬로', channel);
-	} else {
-		rtm.sendMessage('응?', channel);
-	}
+rtm.on('message', (message) => {
+  const { channel } = message;
+  const { text } = message;
+  console.log(channel);
+  if (text == '안녕') {
+    rtm.sendMessage('헬로', channel);
+  } else {
+    rtm.sendMessage(channel, channel);
+    rtm.sendMessage('응?', channel);
+  }
 });
--- index3.js
+++ index3.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
 const { RTMClient } = require('@slack/rtm-api');
 
-var token = process.env.SLACK_TOKEN;
+const token = process.env.SLACK_TOKEN;
 
-var rtm = new RTMClient(token);
+const rtm = new RTMClient(token);
 rtm.start();
 
-var food = require('./food');
-var movie = require('./movie');
+const food = require('./food');
+const movie = require('./movie');
 
-rtm.on('message', function (message){
-	var channel = message.channel;
-	var text = message.text;
+rtm.on('message', (message) => {
+  const { channel } = message;
+  const { text } = message;
 
-	switch(text) {
-		case '영화' :
-			movie(rtm,channel);
-			break;
-		case '밥':
-			food(rtm,channel);
-			break;
-		case '놀이':
-			rtm.sendMessage('그만해.', channel);
-			break;
-		default:
-			rtm.sendMessage('안녕하세요. 영화,밥,놀이 중에 말씀해주세요.',channel);
-	}
+  switch (text) {
+  case '영화':
+    movie(rtm, channel);
+    break;
+  case '밥':
+    food(rtm, channel);
+    break;
+  case '놀이':
+    rtm.sendMessage('그만해.', channel);
+    break;
+  default:
+    rtm.sendMessage('안녕하세요. 영화,밥,놀이 중에 말씀해주세요.', channel);
+  }
 });
