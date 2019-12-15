2d2db5b1f6d5ca76fb3cc69d09f8294bd268aa87
Insu Jeong <wjddlstn0614@naver.com>
2019-12-15 19:48:33 +0900

    Update index3.js

--- index3.js
+++ index3.js
@@ -23,6 +23,9 @@ rtm.on('message', (message) => {
   case '놀이':
     rtm.sendMessage('그만해.', channel);
     break;
+  case '잠':
+    rtm.sendMessage('잘자.', channel);
+    break;
   default:
     rtm.sendMessage('안녕하세요. 영화,밥,놀이 중에 말씀해주세요.', channel);
   }
